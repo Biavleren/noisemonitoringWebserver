c546820686a03271f7d0e9dd021a6186f7e95e78
Biavleren <71124726+Biavleren@users.noreply.github.com>
2022-10-24 17:14:47 +0200

    insert into table test, hardcoded

--- index.php
+++ index.php
@@ -2,13 +2,25 @@
 $servername = "mysqlms.mysql.database.azure.com";
 $username = "superuser";
 $password = "Password123";
+$dbname = "noise_monitoring";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+
+// Insert data into database
+$sql_quiry = "INSERT INTO measurements (measurementUnit_id, acousticShocks, hoursLeft) VALUES (1, 4, 6.3)";
+
+if ($conn->query($sql_quiry) == TRUE) {
+    echo "New record created succesfully";
+}
+else
+{
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+$conn->close();
 ?>
