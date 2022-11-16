24e4a379fe4812da1bb5c3366cb5f2e69356b043
Biavleren <71124726+Biavleren@users.noreply.github.com>
2022-11-16 21:03:34 +0100

    two queries

--- php/calculateTimeLeft.php
+++ php/calculateTimeLeft.php
@@ -6,7 +6,12 @@ $password = "Password123";
 $dbname = "noise_monitoring";
 
 
+// vi har brug for:
+    // soundPressureLevelRaw_id for alle records i measurements, der er NULL.
 
+    // 
+
+// get all 
 
 
 ?>
