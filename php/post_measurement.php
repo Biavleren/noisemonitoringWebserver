4d67c0a542a7e7a433819b151cfecafb6731f585
Biavleren <71124726+Biavleren@users.noreply.github.com>
2022-11-21 10:56:37 +0100

    test another

--- php/post_measurement.php
+++ php/post_measurement.php
@@ -10,17 +10,46 @@ $dbname = "noise_monitoring";
 //$acousticShocks = $_POST["acousticShocks"];
 //$spl_array = $_POST["spl_array"]; // PHP automatically detects as array
 
+// Only allow POST requests
+if (strtoupper($_SERVER['REQUEST_METHOD']) != 'POST') {
+    throw new Exception('Only POST requests are allowed');
+  }
+
+// Make sure Content-Type is application/json 
+$content_type = isset($_SERVER['CONTENT_TYPE']) ? $_SERVER['CONTENT_TYPE'] : '';
+if (stripos($content_type, 'application/json') === false) {
+  throw new Exception('Content-Type must be application/json');
+}
+
+// Read the input stream
+$body = file_get_contents("php://input");
+ 
+// Decode the JSON object
+$object = json_decode($body, true);
+
+// Throw an exception if decoding failed
+if (!is_array($object)) {
+    throw new Exception('Failed to decode JSON object');
+}
+
+// Display the object
+print_r($object);
+
+/*
+
 // Takes raw data from the request
 $json = file_get_contents('php://input');
 
 // Converts it into a PHP pbject
 $data = json_decode($json, true);
 
-echo "measurementUnit_serialNum: ".$data['measurementUnit_serialNum'];
-echo "acousticShocks: ".$data->acousticShocks;
-echo "spl_array[0]: ".$data->spl_array[0];
-echo "spl_array[1]: ".$data->spl_array[1];
-echo "spl_array[2]: ".$data->spl_array[2];
+echo "measurementUnit_serialNum: " . $data['measurementUnit_serialNum'];
+echo "acousticShocks: " . $data->acousticShocks;
+echo "spl_array[0]: " . $data->spl_array[0];
+echo "spl_array[1]: " . $data->spl_array[1];
+echo "spl_array[2]: " . $data->spl_array[2];
+
+*/
 
 // echo "measurementUnit_serialNum: ".$measurementUnit_serialNum;
 // echo "acousticShocks: ".$acousticShocks;
