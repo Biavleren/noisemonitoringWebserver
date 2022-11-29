2c6a30bba1084bdfaa971b960573e1a11d5f8763
Biavleren <71124726+Biavleren@users.noreply.github.com>
2022-11-29 17:55:54 +0100

    with array

--- php/post_measurement.php
+++ php/post_measurement.php
@@ -10,23 +10,30 @@ $measurementUnit_serialNum = $_POST["measurementUnit_serialNum"];
 $acousticShocks = isset($_POST["acousticShocks"]) ? $_POST["acousticShocks"] : 0;
 $spl_length = isset($_POST["spl_length"]) ? $_POST["spl_length"] : 0;
 
-$spl_array0 = $_POST["spl_array0"];
-$spl_array1 = $_POST["spl_array1"];
-$spl_array2 = $_POST["spl_array2"];
-$spl_array3 = $_POST["spl_array3"];
-$spl_array4 = $_POST["spl_array4"];
-$spl_array5 = $_POST["spl_array5"];
-$spl_array6 = $_POST["spl_array6"];
-$spl_array7 = $_POST["spl_array7"];
-$spl_array8 = $_POST["spl_array8"];
-$spl_array9 = $_POST["spl_array9"];
+$spl_array = array();
+
+for ($c = 0; $c < $spl_length; $c++)
+{
+    array_push($spl_array, $_POST["spl_array$c"]);
+}
+
+// $spl_array0 = $_POST["spl_array0"];
+// $spl_array1 = $_POST["spl_array1"];
+// $spl_array2 = $_POST["spl_array2"];
+// $spl_array3 = $_POST["spl_array3"];
+// $spl_array4 = $_POST["spl_array4"];
+// $spl_array5 = $_POST["spl_array5"];
+// $spl_array6 = $_POST["spl_array6"];
+// $spl_array7 = $_POST["spl_array7"];
+// $spl_array8 = $_POST["spl_array8"];
+// $spl_array9 = $_POST["spl_array9"];
 
 echo "measurementUnit_serialNum: ".$measurementUnit_serialNum;
 echo "acousticShocks: ".$acousticShocks;
 echo "spl_length: ".$spl_length;
-echo "spl_array[0]: ".$spl_array0;
-echo "spl_array[1]: ".$spl_array1;
-echo "spl_array[2]: ".$spl_array2;
+for ($j = 0; $j < $spl_length; $j++) {
+    echo "spl_array[$j]: ".$spl_array[$j];
+  }
 
 // if not null, proceed
 if (isset($measurementUnit_serialNum)) {
